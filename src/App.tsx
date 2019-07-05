83d2ccebd0c73a707158b1324d238de394ace3ac
LoLoCrO <bosniclovro@gmail.com>
2019-07-05 11:41:11 +0200

    fetching from apicompleted

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,26 @@ import styled from 'styled-components';
 const App: React.FC = () => {
   const [textColor, setTextColor] = useState<string>('yellow');
   const [text, setText] = useState<string>('type here');
-
-
-  
+ 
   return (
     <React.Fragment>
       <Text textColor={textColor} text={text}></Text>
-      <input value={text} onChange={e => setText(e.target.value)}></input>
+      <Input value={text} onChange={e => setText(e.target.value)} />
     </React.Fragment>
-
   );
 }
 
 export default App;
+
+const Input = styled.input`
+  width: 15rem;
+  text-align: center;
+  padding: 10px;
+  margin: 20px auto;
+  border: 2px solid gray;
+  border-radius: 4px;
+  justify-self: center; 
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
--- src/components/text.tsx
+++ src/components/text.tsx
@@ -8,14 +8,26 @@ interface TextProps {
 
 const Text: React.FC<TextProps> = ({ textColor, text }) => {
     const [color, setColor] = useState<string>(textColor);
+    const randomColorURL = `http://www.colr.org/json/color/random`;
 
-    return <Paragraph color={color}>{text}</Paragraph>
+    const randomColor = () => {
+        fetch(randomColorURL)
+            .then(res => res.json())
+            .then(data => setColor(`#` + (data.new_color || `000`)));
+    }
+
+    return <Paragraph textColor={color} onClick={e => randomColor()}>{text}</Paragraph>
 }
 
 export default Text;
-// color: ${({ textColor }: TextProps) => textColor};
+
+
+interface ParagraphProps {
+    textColor: string;
+}
 
 const Paragraph = styled.p`
+color: ${({ textColor }: ParagraphProps) => textColor};
 border: 2px solid gray;
 padding: 10px;
 border-radius: 10px;
